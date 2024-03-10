d11f6930dc7499ff9a8238c4d9224ce869d980e5
Nikita <gauss54321@gmail.com>
2024-03-10 17:21:27 +0300

    DB

---
+++ my-schedule.sql
@@ -0,0 +1,462 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 10 2024 г., 15:19
+-- Версия сервера: 10.4.28-MariaDB
+-- Версия PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `my-schedule`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `12`
+--
+
+CREATE TABLE `12` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `13`
+--
+
+CREATE TABLE `13` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `14`
+--
+
+CREATE TABLE `14` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `15`
+--
+
+CREATE TABLE `15` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `21`
+--
+
+CREATE TABLE `21` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `22`
+--
+
+CREATE TABLE `22` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `23`
+--
+
+CREATE TABLE `23` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `24`
+--
+
+CREATE TABLE `24` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `25`
+--
+
+CREATE TABLE `25` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `26`
+--
+
+CREATE TABLE `26` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `31`
+--
+
+CREATE TABLE `31` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `32`
+--
+
+CREATE TABLE `32` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `33`
+--
+
+CREATE TABLE `33` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `34`
+--
+
+CREATE TABLE `34` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `35`
+--
+
+CREATE TABLE `35` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Дамп данных таблицы `35`
+--
+
+INSERT INTO `35` (`Time`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`) VALUES
+('1) 8.00 - 8.45', 'БЖ(Кол)-БЖ(Фад)', 'Практика(Викт)', 'Станд.(Карп)', 'Станд.(Карп)', '-', '-/Ин.яз(Горд)'),
+('2) 8.55 - 9.40', 'БЖ(Кол)-БЖ(Фад)', 'Практика(Викт)', 'ИС(Сог)', 'Станд.(Карп)', '-', '-/Ин.яз(Горд)'),
+('3) 9.50 - 10.35', 'ДМ(Карп)', 'ИС(Сог)', 'ИС(Сог)', 'Физра', 'ИС(Сог)', 'МДК 08.01(Викт)'),
+('4) 10.45 - 11.30', 'ДМ(Карп)', 'ИС(Сог)', 'ДМ(Карп)', 'Физра', 'ИС(Сог)', 'МДК 08.01(Викт)'),
+('Обед', '-', '-', '-', '-', '-', '-'),
+('5) 12.15 - 13.00', 'Ин-яз(Анис)/-', 'ЧМ(Карп)', 'УП по ПМ 05', 'МДК 08.01(Викт)', 'УП по ПМ 05', 'Практика(Викт)'),
+('6) 13.10 - 13.55', 'Ин-яз(Анис)/-', 'ЧМ(Карп)', 'УП по ПМ 05', 'МДК 08.01(Викт)', 'УП по ПМ 05', '-'),
+('7) 14.05 - 14.50', '-', 'УП по ПМ 05', 'Кл. час', 'ЧМ(Карп)', 'УП по ПМ 05', '-'),
+('8) 15.00 - 15.45', '-', 'УП по ПМ 05', '-', '-', '-', '-'),
+('9) 15.55 - 16.40', '-', '-', '-', '-', '-', '-'),
+('10) 16.50 - 17.35', '-', '-', '-', '-', '-', '-'),
+('11) 17.45 - 18.30', '-', '-', '-', '-', '-', '-'),
+('12) 18.40 - 19.25', '-', '-', '-', '-', '-', '-');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `36`
+--
+
+CREATE TABLE `36` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `41`
+--
+
+CREATE TABLE `41` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `42`
+--
+
+CREATE TABLE `42` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `43`
+--
+
+CREATE TABLE `43` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `44`
+--
+
+CREATE TABLE `44` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `45`
+--
+
+CREATE TABLE `45` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `46`
+--
+
+CREATE TABLE `46` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `1116`
+--
+
+CREATE TABLE `1116` (
+  `Time` varchar(50) NOT NULL,
+  `Monday` varchar(100) DEFAULT NULL,
+  `Tuesday` varchar(100) DEFAULT NULL,
+  `Wednesday` varchar(100) DEFAULT NULL,
+  `Thursday` varchar(100) DEFAULT NULL,
+  `Friday` varchar(100) DEFAULT NULL,
+  `Saturday` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Дамп данных таблицы `1116`
+--
+
+INSERT INTO `1116` (`Time`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`) VALUES
+('100', '-', '-', '-', '-', '-', '-');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id_u` int(11) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id_u`, `login`, `password`, `status`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
+(3, 'techer', '3360fe6a1cb0cf87d94071081345bffc', NULL);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_u`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_u` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
