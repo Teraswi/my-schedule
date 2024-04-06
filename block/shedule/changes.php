7947e8baf7dec4bbd17ee0c4931614c398150dcb
Nikita <gauss54321@gmail.com>
2024-04-06 16:06:48 +0300

    Update changes

--- block/shedule/changes.php
+++ block/shedule/changes.php
@@ -1,268 +1,38 @@
+<?php
+  $show = "SHOW TABLES LIKE '!%'"; // Вытаскиваем все таблицы, которые наинаются с !
+  $result = mysqli_query($link, $show) or die("Не возможно выполнить запрос");
+  $rows=mysqli_num_rows($result);
 
-<h1>Изменения в расписании на 01.12 (пятница) 2023/2024 учебного года</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Группа</th>
-          <th>12</th>
-          <th>13</th>
-          <th>34</th>
-          <th>41</th>
-          <th>24</th>
-          <th>46</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1) 8.00 - 8.45</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>По расписанию</td>
-          <td>По расписанию</td>
-        </tr>
-        <tr>
-          <td>2) 8.55 - 9.40</td>
-          <td>По расписанию</td>
-          <td>&nbsp;</td>
-          <td>По расписанию</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>3) 9.50 - 10.35</td>
-          <td>ДМ(Карп)</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>По расписанию</td>
-          <td>История(Бл.) 2</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>4) 10.45 - 11.30</td>
-          <td>ДМ(Карп)</td>
-          <td>По расписанию</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>История (Бл) 2</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>Обед</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>5) 12.15 - 13.00</td>
-          <td>Ин&nbspяз(Анис)/&nbsp</td>
-          <td>По расписанию</td>
-          <td>По расписанию</td>
-          <td>По расписанию</td>
-          <td>По расписанию</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>6) 13.10 - 13.55</td>
-          <td>Русс.яз (Фед)</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>7) 14.05 - 14.50</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>8) 15.00 - 15.45</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>9) 15.55 - 16.40</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>10) 16.50 - 17.35</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>11) 17.45 - 18.30</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>12) 18.40 - 19.25</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-      </tbody>
-    </table>
+  for ($i = 0; $i < $rows; $i++) {
+    $row = mysqli_fetch_row($result);
+    $table = $row[0]; // Заносим название таблицы
 
-    <!-- <h1>Изменения в расписании на 22.11 (Среда) 2023/2024 учебного года</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Группа</th>
-          <th>12</th>
-          <th>13</th>
-          <th>34</th>
-          <th>41</th>
-          <th>24</th>
-          <th>46</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1) 8.00 - 8.45</td>
-          <td>-</td>
-          <td></td>
-          <td>-</td>
-          <td>-</td>
-          <td>По расписанию</td>
-          <td>По расписанию</td>
-        </tr>
-        <tr>
-          <td>2) 8.55 - 9.40</td>
-          <td>По расписанию</td>
-          <td></td>
-          <td>По расписанию</td>
-          <td>-</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>3) 9.50 - 10.35</td>
-          <td>ДМ(Карп)</td>
-          <td></td>
-          <td></td>
-          <td>По расписанию</td>
-          <td>История(Бл.) 2</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>4) 10.45 - 11.30</td>
-          <td>ДМ(Карп)</td>
-          <td>По расписанию</td>
-          <td></td>
-          <td></td>
-          <td>История (Бл) 2</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Обед</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>5) 12.15 - 13.00</td>
-          <td>Ин-яз(Анис)/-</td>
-          <td>По расписанию</td>
-          <td>По расписанию</td>
-          <td>По расписанию</td>
-          <td>По расписанию</td>
-          <td>-</td>
-        </tr>
-        <tr>
-          <td>6) 13.10 - 13.55</td>
-          <td>Русс.яз (Фед)</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>7) 14.05 - 14.50</td>
-          <td></td>
-          <td></td>
-          <td>-</td>
-          <td></td>
-          <td>-</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>8) 15.00 - 15.45</td>
-          <td></td>
-          <td>-</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>9) 15.55 - 16.40</td>
-          <td></td>
-          <td>-</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>10) 16.50 - 17.35</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>11) 17.45 - 18.30</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>12) 18.40 - 19.25</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table> -->
+    $tablesql = "SELECT * FROM `$table`";
+    $tableRes = mysqli_query($link, $tablesql) or die("Ошибка"); // Выносим данные каждой таблицы
+
+    echo "<h1>Изменения в расписании на $table 2023/2024 учебного года</h1>";
+    echo "<table>";
+    echo "<thead><tr>";
+    
+    $show_collumn = "SHOW COLUMNS FROM `$table`"; //Выносим названия столбцов
+    $show_res_coll = mysqli_query($link, $show_collumn);
+
+    while($row = mysqli_fetch_assoc($show_res_coll)) {
+        echo "<td>{$row['Field']}</td>";
+    }
+    
+    echo "</tr></thead>";
+    echo "<tbody>";
+
+    while ($table_data = mysqli_fetch_row($tableRes)) {
+        echo "<tr>";
+        foreach ($table_data as $data) {
+            echo "<td>$data</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</tbody>";
+    echo "</table>";
+}
+?>
